53aaaf38cb5f08f8d927695d1c374263bf3d2ea4
André <75815203+datSunky@users.noreply.github.com>
2021-05-05 21:50:59 +0100

    a

--- project/main.c
+++ project/main.c
@@ -1,6 +1,17 @@
-#include "handlers\headers.h"
+/***********************************************************\
+EI - Programação 1
+PL1 - Gestão Propostas de Crédito
+Realizado por: André Brandão (26244) e Diogo Campos (xxxxx)
+\***********************************************************/
+
+//HEADER
+#include "handlers\headers.h" 
+
+//HANDLERS
 #include "handlers\structs.c"
 #include "handlers\test.c"
+
+//MAIN
 int main(int argc, char const *argv[])
 {
     printf("Hello World!\n");
